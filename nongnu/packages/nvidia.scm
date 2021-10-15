dff673a20968e2f90a32e597f7ae7169f5e22451
Antwane Mason <ad.mason1413@gmail.com>
2021-10-15 15:55:20 -0400

    First signed commit

--- nongnu/packages/nvidia.scm
+++ nongnu/packages/nvidia.scm
@@ -20,6 +20,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 (define-module (nongnu packages nvidia)
   #:use-module (guix packages)
   #:use-module (guix download)
